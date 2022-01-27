0ca017447f1fa7ecd65da7c1fadf3481bfbe1422
BHASKAR KUMAR <84345649+bhaskarkumar1@users.noreply.github.com>
2022-01-27 15:55:00 +0530

    Create string.js

---
+++ string.js
@@ -0,0 +1,12 @@
+var message="Hello!";
+var name="Bhaskar";
+// alert(message +" there "+ name);
+// console.log(name.length);
+// var input= prompt("Compose Your tweet: ");
+// console.log(280-input.length + " Character left ")
+
+// console.log("So your tweet is :" + input.slice(0,280));
+
+// to upper case
+name =name.toUpperCase();
+name=name.toLowerCase();
